4b7d87e6159361719f8ac174c0f1d921456d877a
Sarfaraz Jaffery <83608906+jafferytech110@users.noreply.github.com>
2021-08-17 00:49:14 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ if (window.innerWidth < 700) {
     fontAwesome[i].classList.remove("fa-4x");
     fontAwesome[i].classList.add("fa-3x");
   }
-}
+};
 if (window.innerWidth < 400) {
   for (i = 1; i < fontAwesome.length; i++) {
     fontAwesome[i].classList.remove("fa-4x");
     fontAwesome[i].classList.remove("fa-3x");
     fontAwesome[i].classList.add("fa-2x");
   }
-}
+};
